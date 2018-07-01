ce3a9d005f63a6c314ac295ac29e2a78998303cb
Andersh75 <andersh75@gmail.com>
2018-07-02 01:56:26 +0200

    Build

--- dist/index.js
+++ dist/index.js
@@ -16,48 +16,45 @@ class WhcgLitTextField extends litElement.LitElement {
       period: String
     };
   }
-  onBlur(e) {
+
+  _render({ label, placeholder, value, suffix }) {
+    return litElement.html`
+      <vaadin-text-field label="${label}" value="${value}" placeholder="${placeholder}" theme="align-right" on-blur="${this._onBlur.bind(
+      this
+    )}" on-click="${this._onClick.bind(this)}" on-input="${this._onInput.bind(
+      this
+    )}" on-change="${this._onChange.bind(this)}">
+    <div slot="suffix">${suffix}</div>
+    </vaadin-text-field>
+    `;
+  }
+
+  _onBlur(e) {
     let event = new CustomEvent("blur-changed", {
       detail: { value: e.path[4].value, input: e.data }
     });
     this.dispatchEvent(event);
   }
-  onClick(e) {
-    let event = new CustomEvent("click-changed", {
-      detail: { value: e.path[4].value }
-    });
-    this.dispatchEvent(event);
-  }
-  onInput(e) {
-    let event = new CustomEvent("input-changed", {
-      detail: { value: e.path[4].value, input: e.data }
-    });
-    this.dispatchEvent(event);
-  }
 
-  onChange(e) {
+  _onChange(e) {
     let event = new CustomEvent("change-changed", {
       detail: { value: e.path[0].value }
     });
     this.dispatchEvent(event);
   }
 
-  _render({ label, placeholder, value, suffix }) {
-    return litElement.html`
-      <style>
-        button, input {
-          border: 1px solid black;
-        }
-      </style>
+  _onClick(e) {
+    let event = new CustomEvent("click-changed", {
+      detail: { value: e.path[4].value }
+    });
+    this.dispatchEvent(event);
+  }
 
-      <vaadin-text-field label="${label}" value="${value}" placeholder="${placeholder}" theme="align-right" on-blur="${this.onBlur.bind(
-      this
-    )}" on-click="${this.onClick.bind(this)}" on-input="${this.onInput.bind(
-      this
-    )}" on-change="${this.onChange.bind(this)}">
-    <div slot="suffix">${suffix}</div>
-    </vaadin-text-field>
-    `;
+  _onInput(e) {
+    let event = new CustomEvent("input-changed", {
+      detail: { value: e.path[4].value, input: e.data }
+    });
+    this.dispatchEvent(event);
   }
 }
 
