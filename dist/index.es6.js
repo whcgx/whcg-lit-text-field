bc90730a258baebab688742bf21995e53ad12af1
Andersh75 <andersh75@gmail.com>
2018-08-02 15:36:32 +0200

    Build

--- dist/index.es6.js
+++ dist/index.es6.js
@@ -1,15 +1,20 @@
+import '@vaadin/vaadin-text-field';
 import '@whcg/whcg-lit-text-field/theme/whcg-lit-text-field-styles.js';
-import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 class WhcgLitTextField extends PolymerElement {
     static get template() {
         return html `
-        <style include="whcg-lit-text-field-style-default-theme">
-            vaadin-text-field {
-                --material-body-text-color: var(--lit-text-field-color);
-            }
+        <style>
+             vaadin-text-field {
+                --text-field-input-field-bg-color: var(--parmaco-text-field-label-text-color);
+             }
+            
+
         </style>
+
+
+        <h2>THIS IS H2</h2>
         <vaadin-text-field value="thevalue" label="thelabel"></vaadin-text-field>
     `
     };   
--- dist/index.js
+++ dist/index.js
@@ -2,18 +2,23 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+require('@vaadin/vaadin-text-field');
 require('@whcg/whcg-lit-text-field/theme/whcg-lit-text-field-styles.js');
-require('@vaadin/vaadin-text-field/src/vaadin-text-field.js');
 var polymerElement_js = require('@polymer/polymer/polymer-element.js');
 
 class WhcgLitTextField extends polymerElement_js.PolymerElement {
     static get template() {
         return polymerElement_js.html `
-        <style include="whcg-lit-text-field-style-default-theme">
-            vaadin-text-field {
-                --material-body-text-color: var(--lit-text-field-color);
-            }
+        <style>
+             vaadin-text-field {
+                --text-field-input-field-bg-color: var(--parmaco-text-field-label-text-color);
+             }
+            
+
         </style>
+
+
+        <h2>THIS IS H2</h2>
         <vaadin-text-field value="thevalue" label="thelabel"></vaadin-text-field>
     `
     };   
